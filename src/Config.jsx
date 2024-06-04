5dac5510a9a2f595f9762299e4584d7ebf37bc06
92JosephLim <156063955+92JosephLim@users.noreply.github.com>
2024-06-04 12:05:25 +0900

    Delete src directory

--- src/Config.jsx
+++
@@ -1,3 +0,0 @@
-//SERVER ROUTES
-export const API_URL = 'http://apis.data.go.kr/6260000/FoodService/getFoodKr';
-export const API_KEY = 'Gp0K6TjqqzvLQmwsUON5mQqNrXkR1Hs%2FS2CUpwV6lR2ONJ0b1eUmQdlQ22MfB3NgNhiU8nj80Qkvn3haEWGBgA%3D%3D';
