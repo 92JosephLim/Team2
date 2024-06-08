da3c25f813df3acfd368f73aab68c65dc949e78e
noonayasalido <156063875+noonayasalido@users.noreply.github.com>
2024-06-08 16:51:25 +0900

    hehe

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,6 +1,3 @@
-
-import React from "react";
-
 // //마이페이지 : 좌측 네비게이션
 // SideNav.js
 import React, { useState } from "react";
@@ -76,37 +73,6 @@ function SideNav() {
                 <span className="text-white">설정</span>
               </a>
             </li> */}
-            <li className="rounded-sm">
-              <Link to="/ProfileSettings" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
-                <span>상세 프로필 설정</span>
-              </Link>
-            </li>
-            <li className="rounded-sm">
-              <Link to="/chat-history" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
-                <span>채팅 내역</span>
-              </Link>
-            </li>
-            <li className="rounded-sm">
-              <Link to="/video-chat-history" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
-                <span>화상채팅 내역</span>
-              </Link>
-            </li>
-            <li className="rounded-sm">
-              <div className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
-                <div className="dropdown">
-                  <button className="dropdown-button">친구</button>
-                  <div className="dropdown-content">
-                    <Link to="/invite">친구추가</Link>
-                    <Link to="/friendMain">친구목록</Link>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li className="rounded-sm">
-              <Link to="/settings" className="flex items-center p-2 space-x-3 rounded-md hover:bg-gray-700">
-                <span>설정</span>
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
