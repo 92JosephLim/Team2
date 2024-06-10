3c717f71cac205d0360ee136b2b8811f8eb72adb
jiyeonPark <qkrwluds7998@gmail.com>
2024-06-10 15:50:28 +0900

    edit routing

--- src/App.js
+++ src/App.js
@@ -16,13 +16,6 @@ import InviteFriend from "./pages/mypage/InviteFriend";
 import FriendMain from "./pages/mypage/FriendMain";
 import MyPage from "./pages/MyPage";
 import ProfileSettings from "./pages/mypage/ProfileSettings"; // 새로 추가한 ProfileSettings 컴포넌트
-<<<<<<< HEAD
-import Translation from "./components/Translation";//testing용 번역
-=======
-import Translation from "./components/Translation";
-import KakaoRedirect from "./pages/social/KakaoRedirect";
-import JoinRoom from "./pages/JoinRoom";
->>>>>>> 1d76d5f4949ef94a39cf619127c2bcb5e81754b3
 
 const clientId = "233505782576-acmbig2ssomblm8c8spashbrj6004jdl.apps.googleusercontent.com";
 
@@ -46,7 +39,6 @@ function App() {
             <Route path="/friendMain" element={<FriendMain />} />
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/ProfileSettings" element={<ProfileSettings />} />
-            <Route path="/translate" element={<Translation />} />
           </Routes>
         </BrowserRouter>
       </div>
